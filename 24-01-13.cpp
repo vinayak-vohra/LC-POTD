3ce11141b16574fca8002d634c28be760cc83d66
Vinayak Vohra <vn26vhr@gmail.com>
2024-01-20 19:46:36 +0530

    T: 44ms[83.77%] M: 16.86mb[80.17%]

---
+++ 24-01-13.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minSteps(string s, string t)
+    {
+        vector<int> charCount(26, 0);
+
+        for (int i = 0; i < s.size(); i++)
+            charCount[s[i] - 'a']--, charCount[t[i] - 'a']++;
+
+        int ans = 0;
+        for (auto &x : charCount)
+            ans += (x > 0) ? x : 0;
+
+        return ans;
+    }
+};
