c56e1d36c7d084f85c8cfe2a7ed603550f888229
Vinayak Vohra <vn26vhr@gmail.com>
2024-01-20 19:42:16 +0530

    T: 0ms[100%] M: 8.57mb[75.42%]

---
+++ 24-01-17.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int> &arr)
+    {
+        unordered_map<int, int> count;
+
+        for (int &x : arr)
+            count[x]++;
+
+        unordered_set<int> x;
+        for (auto &mp : count)
+            if (!(x.insert(mp.second).second))
+                return false;
+
+        return true;
+    }
+};
