dc712b4e897b1a26ae0312762b012438bd99193f
Vinayak Vohra <vn26vhr@gmail.com>
2024-01-20 19:58:25 +0530

    T: 16ms[98.01%] M: 17.89mb[37.75%]

---
+++ 24-01-01.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s)
+    {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int count = 0;
+        auto gi = g.begin();
+        auto si = s.begin();
+
+        while (gi < g.end() && si < s.end())
+        {
+            if (*gi <= *si)
+                count++, gi++;
+
+            si++;
+        }
+        return count;
+    }
+};
