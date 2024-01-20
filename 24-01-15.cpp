a2650afcde0a280be6cbb29a3a983ff173642a56
Vinayak Vohra <vn26vhr@gmail.com>
2024-01-20 19:44:08 +0530

    T: 422ms[84.12%] M: 165.3mb[61.52%]

---
+++ 24-01-15.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> findWinners(vector<vector<int>> &matches)
+    {
+        unordered_map<int, int> won;
+        unordered_map<int, int> lost;
+        vector<vector<int>> res(2, vector<int>());
+
+        for (auto &match : matches)
+        {
+            won[match[0]]++;
+            lost[match[1]]++;
+        }
+
+        for (auto &x : won)
+        {
+            if (lost.find(x.first) == lost.end())
+                res[0].push_back(x.first);
+        }
+
+        for (auto &x : lost)
+        {
+            if (x.second == 1)
+                res[1].push_back(x.first);
+        }
+        sort(res[0].begin(), res[0].end());
+        sort(res[1].begin(), res[1].end());
+
+        return res;
+    }
+};
